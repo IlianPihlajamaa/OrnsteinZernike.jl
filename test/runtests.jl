7e05490bca8e7e5c0a6c78ea96de3493e934b5e3
Ilian Pihlajamaa <ilian.pihlajamaa@gmail.com>
2023-08-10 12:02:13 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test, OrnsteinZernike, StaticArrays
 using FFTW
 
-for target in ["Fourier", "FourierIteration_HS", "NGIteration_HS", "thermodynamics", "DensityRamp"]
+for target in ["Fourier", "FourierIteration_HS", "NgIteration_HS", "thermodynamics", "DensityRamp"]
     @testset "$target" begin
         include("test_$target.jl")
     end
