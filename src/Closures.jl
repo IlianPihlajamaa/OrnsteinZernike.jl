0e08161c475da774edacb2946a119a7732b868cc
IlianPihlajamaa <73794090+IlianPihlajamaa@users.noreply.github.com>
2023-09-21 14:53:16 +0200

    Typo in Verlet closure

--- src/Closures.jl
+++ src/Closures.jl
@@ -74,7 +74,7 @@ end
 """
     Verlet <: Closure
 
-Implements the Verlet Closure \$b(r) = -\\frac{A\\gamma^2(r)/2}{1+B \\gamma(r)/2}, where by default \$A=1\$ and \$B=8/5\$. These values are tuned by the virial coefficients of the 3d hard sphere liquid.
+Implements the Verlet Closure \$b(r) = -\\frac{A\\gamma^2(r)/2}{1+B \\gamma(r)/2}\$, where by default \$A=1\$ and \$B=8/5\$. These values are tuned by the virial coefficients of the 3d hard sphere liquid.
 
 Example:
 ```julia
